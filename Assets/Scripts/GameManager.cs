99e6907a21c21d1c44d956561637a55c78d7b7e6
Michael Campos <mjcampos@users.noreply.github.com>
2025-04-14 16:48:42 -0700

    Update GameManager.cs
    Made it persist. Going to be useful for switching between main menu and game scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,17 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    
+    public static GameManager Instance;
+
+    void Awake() {
+        // Singleton pattern to ensure only one GameManager exists
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
 }
