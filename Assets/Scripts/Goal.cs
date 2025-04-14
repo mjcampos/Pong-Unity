12d90edc5b0d9e1fc13dacbed48ad339a2450822
Michael Campos <mjcampos@users.noreply.github.com>
2025-04-14 15:51:45 -0700

    Set up goals and their scripts

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+    int countdownTime = 1;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(CountdownRoutine());
+    }
+
+    IEnumerator CountdownRoutine()
+    {
+        yield return new WaitForSeconds(countdownTime);
+        SceneManager.LoadScene(0);
+    }
+}
